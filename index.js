9c06ed3046573f1f0c0f29e75e1c4bcbdb4d7769
tracycrossrvt-gif <tracycrossrvt@gmail.com>
2026-05-16 14:05:16 -0700

    Eigth Commit

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
+function toggleContrast() {
+  document.body.classList.toggle("dark-theme");
+}
+
 async function contact(event) {
     event.preventDefault();
 
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
-    loading.classList.add('modal__overlay--visible');
-    loading.classList += ' modal__overlay--visible';
+    const error = document.querySelector('.modal__overlay--error');
 
-    await emailjs.sendForm(
-        'service_7390c3r',
-        'template_752zz5b',
-        event.target,
-        'CRPour4rksWN7eSjg'
-    );
+    loading.classList.add('modal__overlay--visible');
+
+    try {
+        await emailjs.sendForm(
+            'service_7390c3r',
+            'template_752zz5b',
+            event.target,
+            'CRPour4rksWN7eSjg'
+        );
+
+        loading.classList.remove('modal__overlay--visible');
+
+        success.classList.add('modal__overlay--visible');
+
+    } catch (err) {
+
+        loading.classList.remove('modal__overlay--visible');
+
+        error.classList.add('modal__overlay--visible');
+
+        console.error(err);
+    }
 }
+
+
+let isModalOpen = false;
+function toggleModal() {
+
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove('modal--open');
+      
+    }
+    isModalOpen = true;
+    document.body.classList += ' modal--open';
+}
+
